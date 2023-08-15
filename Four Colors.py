e612aa5a3951be22e67d3e88277d97f5cc8fa242
GreenRing <141900921+GreyhoundSherperd@users.noreply.github.com>
2023-08-15 18:58:52 +0700

    Add files via upload

--- Four Colors.py
+++ Four Colors.py
@@ -1,3 +1,14 @@
+'''
+Each player gets 20 cards of 4 colors, each color has a different objective:
+- Red: Attack, 1 red card dealt 1 damage if unblocked or countered, play in a zigzag pattern with green cards to multi-combo,
+for example "Red Green Red".
+- Blue: Block, 1 blue card defends 1 damage, can multi-block without green cards.
+- Green: Bridge, Green cards were played in a zigzag pattern with red or yellow cards to multi-combo or multi-counter.
+- Yellow: Counter, A yellow card is a hybrid of red and blue, a yellow card blocks 1 damage and deal 1 damage, can multi-counter without green cards.
+Can only play if a red card was played last turn, and can only play equal or less than the red cards 
+played.
+'''
+
 from random import randint
 from math import floor
 
